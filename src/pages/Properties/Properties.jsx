1c555f0f497671d58430e3be418fc8bd414ace95
Keshav Jadhav <jadavkeshav2005@gmail.com>
2024-03-19 19:30:59 +0530

    initial commit

---
+++ src/pages/Properties/Properties.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Searchbar from '../../components/SearchBar/Searchbar'
+import "./Properties.css"
+import useProperties from '../../hooks/useProperties';
+import {PuffLoader} from "react-spinners"
+import PropertyCard from '../../components/PropertyCard/PropertyCard';
+
+const Properties = () => {
+
+  const {data , isError, isLoading} = useProperties();
+  console.log(data);
+
+  if (isError) {
+    return(
+      <div className="wrapper">
+        Error while fetching data
+      </div>
+    )
+  }
+
+  if (isLoading) {
+    return(
+      <div className="wrapper flexCenter" style={{height: "60vh"}}>
+        <PuffLoader
+          height = "80"
+          width = "80"
+          radius = {1}
+          color='#4066ff'
+          aria-label='puff-loading'
+        />
+      </div>
+    )
+  }
+
+
+  return (
+    <div className='wrapper'>
+      <div className="flexColCenter paddings innerWidth properties-container">
+        <Searchbar/>
+        <div className="padding flexCenter properties">
+          {
+            data.map((card, i)=>(<PropertyCard card={card}/>))
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Properties
